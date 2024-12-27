857afbce1d6ea14e6079bce33b2319f9e8839bfe
AndreyFaichuk <andreyfaichuk@gmail.com>
2024-12-27 17:13:07 +0200

    add logger, custom exception, deleter and get todo by id

--- src/todos/dto/create-todo.dto.ts
+++ src/todos/dto/create-todo.dto.ts
@@ -1,5 +1,10 @@
 import { ApiProperty } from '@nestjs/swagger';
+import mongoose from 'mongoose';
 
+export class Id {
+  @ApiProperty()
+  _id: mongoose.Types.ObjectId;
+}
 export class CreateTodoDto {
   @ApiProperty()
   name: string;
@@ -9,4 +14,9 @@ export class CreateTodoDto {
 
   @ApiProperty()
   progress: number;
+}
+
+export class UpdateTodoDto extends CreateTodoDto {
+  @ApiProperty()
+  _id: Id;
 }
