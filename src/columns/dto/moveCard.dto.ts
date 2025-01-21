b79e0691d408a83babd78e66bc2b4ff7d1cc328e
AndreyFaichuk <andreyfaichuk@gmail.com>
2025-01-21 17:05:36 +0200

    add new field to cards

--- src/columns/dto/moveCard.dto.ts
+++ src/columns/dto/moveCard.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsMongoId, IsInt } from 'class-validator';
 
 export class MoveCardDto {
+  @ApiProperty({ example: '678fa2627ad3f0da1282d374' })
   @IsMongoId()
   fromColumnId: string;
 
+  @ApiProperty({ example: '678fa2627ad3f0da1282d374' })
   @IsMongoId()
   toColumnId: string;
 
+  @ApiProperty({ example: '678fa2627ad3f0da1282d374' })
   @IsMongoId()
   todoId: string;
 
+  @ApiProperty({ example: 0 })
   @IsInt()
   fromTodoIndex: number;
 
+  @ApiProperty({ example: 1 })
   @IsInt()
   toTodoIndex: number;
 }
