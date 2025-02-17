91b7a63f815ebb908e06689abc940b33b2ed54b6
AndreyFaichuk <andreyfaichuk@gmail.com>
2025-02-17 19:36:47 +0200

    add Dockerfile, update README

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   app.use(cookieParser());
 
-  app.enableCors({ credentials: true, origin: 'http://localhost:5173' });
+  app.enableCors({ credentials: true, origin: 'http://localhost:8080' });
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
