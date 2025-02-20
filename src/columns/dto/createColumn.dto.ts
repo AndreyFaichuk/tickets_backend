05823cb44144309f5912f98ce01b7c56ab62f3af
AndreyFaichuk <andreyfaichuk@gmail.com>
2025-02-20 12:01:31 +0200

    fix

--- src/columns/dto/createColumn.dto.ts
+++ src/columns/dto/createColumn.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateColumnDto {
-  @ApiProperty({ example: 'Column name' })
+  @ApiProperty({ example: 'Column name1' })
   title: string;
 }
